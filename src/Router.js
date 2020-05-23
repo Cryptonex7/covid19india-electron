500f89f9170f146bb5e0003b5251bda48d12c3c8
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-05-23 12:19:57 +0530

    code cleaning

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,16 @@
 import React, {lazy} from 'react';
 import {Switch, Route} from 'react-router';
 import suspenseHoc from './hoc/suspenseHoc';
-import headerHoc from './hoc/headerHoc';
 import withStyles from '@material-ui/core/styles/withStyles';
 import './App.scss';
 import {globalStyles} from './globalStyles';
 
-const NewsPage = lazy(() => import('./pages/NewsPage'));
-const QuizPage = lazy(() => import('./pages/QuizPage'));
 const HomePage = lazy(() => import('./pages/HomePage'));
 
 function Router(props) {
   return (
     <Switch>
       <Route exact path="/" component={suspenseHoc(HomePage)} />
-      <Route exact path="/quiz" component={suspenseHoc(headerHoc(QuizPage))} />
-      <Route exact path="/news" component={suspenseHoc(headerHoc(NewsPage))} />
     </Switch>
   );
 }
